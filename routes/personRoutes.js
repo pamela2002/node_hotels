4028d36ff035882d1632ec981dd52615c585e5d8
pamela2002 <sroychoudhury65@gmail.com>
2024-03-27 16:24:36 +0530

    shift mongodb to online server

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -3,6 +3,26 @@ const router = express.Router();
 const Person = require('./../models/Person');
 
 
+
+router.post('/', async (req, res) =>{
+    try{
+        const data = req.body // Assuming the request body contains the person data
+
+        // Create a new Person document using the Mongoose model
+        const newPerson = new Person(data);
+
+        // Save the new person to the database
+        const response = await newPerson.save();
+        console.log('data saved');
+
+        res.status(200).json({response: response});
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
+
 // GET method to get the person
 router.get('/', async (req, res) =>{
     try{
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
 
 const Person = require('./models/Person');
 
@@ -18,6 +20,7 @@ app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
 
 
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 })
